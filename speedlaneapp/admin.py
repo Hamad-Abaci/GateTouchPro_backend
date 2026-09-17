2a04f8ebff1fabd2ffcf865d4e7b6749f0e4ee31
Hamad N M <hamadabacitech@gmail.com>
2026-09-17 17:41:32 +0530

    LANE - CRUD

--- speedlaneapp/admin.py
+++ speedlaneapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Lane,AccessLog
+from .models import Lane,AccessLog,TurnStyle,SystemConfig
 admin.site.register(Lane)
 admin.site.register(AccessLog)
+admin.site.register(TurnStyle)
+admin.site.register(SystemConfig)
